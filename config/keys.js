e0764550a80e128822fe0f57659516beb57245e9
Panchal <APanchal@thegoldensource.com>
2022-02-22 22:47:28 +0530

    first commit

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "",
+    mongoURI: "mongodb+srv://dbUser:dbUser@cluster0.3flln.mongodb.net/mern-auth?retryWrites=true&w=majority",
     secretOrKey: "secret" 
   };
